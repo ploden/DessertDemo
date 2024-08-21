5a425795dea9f1200908a7d41a97d2ee2cd2c118
Phil Loden <ploden@gmail.com>
2024-08-21 16:44:05 -0600

    Initial Commit

---
+++ DessertMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertMenu
+//
+//  Created by Philip Loden on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DessertMenu/DessertMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertMenuApp.swift
+//  DessertMenu
+//
+//  Created by Philip Loden on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
